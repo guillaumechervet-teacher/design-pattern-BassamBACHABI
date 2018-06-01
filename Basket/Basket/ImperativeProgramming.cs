add6df54b79363b7bc1d515202ab402abd787a10
Bassam <bassam@MacBook-Pro-de-Bassam.local>
2018-06-01 16:10:22 +0200

    commit code impératif

---
+++ Basket/Basket/ImperativeProgramming.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Basket
+{
+    public class ImperativeProgramming
+    {
+        public static int CalculateBasketAmount(IList<BasketLineArticle> basketLineArticles)
+        {
+            var amountTotal = 0;
+            foreach (var basketLineArticle in basketLineArticles)
+            {
+                var articleId = basketLineArticle.Id;
+                var article = ArticleDatabase(articleId);
+
+                // Calculate amount
+                var amount = 0;
+                switch (article.Category)
+                {
+                    case "food":
+                        amount += article.Price * 100 + article.Price * 12;
+                        break;
+                    case "electronic":
+                        amount += article.Price * 100 + article.Price * 20 + 4;
+                        break;
+                    case "desktop":
+                        amount += article.Price * 100 + article.Price * 20;
+                        break;
+                }
+
+                amountTotal += amount * basketLineArticle.Number;
+            }
+
+            return amountTotal;
+        }
+
+        private static ArticleDatabase ArticleDatabase(string articleId)
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            var assemblyDirectory = Path.GetDirectoryName(path);
+            var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
+            IList<ArticleDatabase> articleDatabases =
+                JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+            var article = articleDatabases.First(articleDatabase =>
+                articleDatabase.Id == articleId);
+            return article;
+        }
+    }
+}
